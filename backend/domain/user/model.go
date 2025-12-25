f2d85f0341446fa90636787e6d2594af4e76ef22
Ngọc An <148259052+ngocan-dev@users.noreply.github.com>
2025-12-25 15:23:18 +0700

    Fix login handler email authentication

--- backend/domain/user/model.go
+++ backend/domain/user/model.go
@@ -18,8 +18,8 @@ type RegistrationRequest struct {
 
 // LoginRequest represents login request
 type LoginRequest struct {
-	UsernameOrEmail string `json:"username_or_email" binding:"required"`
-	Password        string `json:"password" binding:"required"`
+	Email    string `json:"email" binding:"required"`
+	Password string `json:"password" binding:"required"`
 }
 
 // LoginResponse represents successful login response
